522559e475a371c6ea5ce52d95dfb1060c57d0e0
alinemmmanhaes <aline.manhaes@edu.ufes.br>
2023-09-08 12:19:25 -0300

    exercicio 5

---
+++ 03_bibliotecas/bibli_05/Respostas/Aline/string_utils.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include "string_utils.h"
+
+int string_length(char *str){
+    int i;
+    for(i=0; i<1000; i++){
+        if(str[i] == '\0'){
+            break;
+        }
+    }
+    
+    return i+1;
+}
+
+void string_copy(char *src, char *dest){
+    int i, tam;
+    tam = string_length(src);
+    for(i=0; i<tam; i++){
+        dest[i] = src[i];
+    }
+}
+
+void string_upper(char *str){
+    int i, tam;
+    tam = string_length(str);
+    for(i=0; i<tam; i++){
+        if(str[i]>='a' && str[i]<='z'){
+            str[i] -= 32;
+        }
+    }
+}
+
+void string_lower(char *str){
+    int i, tam;
+    tam = string_length(str);
+    for(i=0; i<tam; i++){
+        if(str[i]>='A' && str[i]<='Z'){
+            str[i] += 32;
+        }
+    }
+}
+
+void string_reverse(char *str){
+    char copia[1000];
+    int tam, i, j;
+    
+    string_copy(str, copia);
+    tam = string_length(str);
+    
+    for(i=tam-2, j=0; i<=0; i--, j++){
+        str[j] = copia[i];
+    }
+}
